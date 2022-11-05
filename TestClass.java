d7cb942788e55b45eb9906c84e992cf37134d5d7
Max <maksatkanat@gmail.com>
2022-11-05 15:28:45 +0600

    test code

--- TestClass.java
+++ TestClass.java
@@ -1,5 +1,9 @@
 public class TestClass {
     public static void main(String[] args) {
         log.info("Hi!");
+        int x = 10;
+        int z = 5;
+        int t = x*t;
+        log.info("t = " + t);
     }
 }
